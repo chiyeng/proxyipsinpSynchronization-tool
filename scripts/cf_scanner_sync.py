4783d3abb309cf0ea658df5864194971b443a786
chiyeng <cylytd@outlook.com>
2026-08-23 11:03:28 +0800

    Update cf_scanner_sync.py

--- scripts/cf_scanner_sync.py
+++ scripts/cf_scanner_sync.py
@@ -107,7 +107,7 @@ def main():
     zone_id = os.environ.get("CF_ZONE_ID")
     target_domain = os.environ.get("CF_TARGET_DOMAIN")
     cf_email = os.environ.get("CF_EMAIL")
-    check_api_url = "https://proxyipsinp.xxxxxxx.nyc.mn/check"
+    check_api_url = "https://op9.wdx.cc.cd/check"
     sync_count = int(os.environ.get("SYNC_COUNT", 5))
     scan_count = int(os.environ.get("SCAN_COUNT", 1000))
     
